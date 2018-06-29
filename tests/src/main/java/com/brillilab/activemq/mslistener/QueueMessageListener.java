1d3300adb592715bed51f78154fc19bfd22d8b7c
WuMenghao <33742731+WuMenghao@users.noreply.github.com>
2018-06-29 17:24:35 +0800

    测试activeMQ的使用

---
+++ tests/src/main/java/com/brillilab/activemq/mslistener/QueueMessageListener.java
@@ -0,0 +1,25 @@
+package com.brillilab.activemq.mslistener;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * 自定义消息监听器
+ *
+ * @author wmh
+ */
+public class QueueMessageListener implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        TextMessage tm= (TextMessage) message;
+        try {
+            System.out.println("QueueMessageListener监听到了文本消息：\t"
+                    + tm.getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+            System.out.println("消息接收失败");
+        }
+    }
+}
