d46ff3d3387f2ac085f139a9f5ea435ee2a714bc
mwu <menghao.wu@duyansoft.com>
2020-08-17 19:18:22 +0800

    redis test

---
+++ src/main/java/com/brillilab/redis/RedisUtils.java
@@ -0,0 +1,15 @@
+package com.brillilab.redis;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.JedisPool;
+
+public class RedisUtils {
+
+    private static final JedisPool jedisPool = null;
+
+    static {
+        new GenericObjectPoolConfig();
+        
+    }
+
+}
