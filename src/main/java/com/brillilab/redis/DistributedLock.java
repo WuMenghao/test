139e89bc4c20d9f57113857de3997695fc1d57bb
mwu <menghao.wu@duyansoft.com>
2020-08-18 10:50:44 +0800

    DistributedLock

--- src/main/java/com/brillilab/redis/DistributedLock.java
+++ src/main/java/com/brillilab/redis/DistributedLock.java
@@ -1,12 +1,16 @@
 package com.brillilab.redis;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.math3.random.RandomGeneratorFactory;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.apache.xmlbeans.impl.tool.CodeGenUtil;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
+import redis.clients.jedis.params.SetParams;
 
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
@@ -14,32 +18,46 @@ public class DistributedLock {
 
     private static JedisPool jedisPool;
 
-    private static final String SUCCESS = "OK";
+    private static final String LOCK_SUCCESS = "OK";
+    private static final Integer UNLOCK_SUCCESS = 1;
     private static final String SET_IF_NOT_EXIST = "NX";
     private static final String SET_WITH_EXPIRE_TIME = "PX";
-    private static final int DEFAULT_EXPIRE_TIME = 15;
+    private static final int DEFAULT_EXPIRE_TIME = 20;
 
     static {
         GenericObjectPoolConfig config = new GenericObjectPoolConfig();
         config.setMaxIdle(10);
-        config.setMaxTotal(20);
-        config.setMinIdle(10);
-        jedisPool=new JedisPool(config,"localhost",6379,1000);
+        config.setMaxTotal(200);
+        config.setMinIdle(10*1000);
+        config.setTestOnReturn(true);
+        jedisPool=new JedisPool(config,
+                "localhost",
+                6379,
+                1000*10,
+                null,
+                10,
+                "test_client;",
+                false);
     }
 
     /**
      * 获取锁
      * @param lockName
      */
-    public static void lock(String lockName){
+    public static String lock(String lockName){
         Jedis jedis = null;
         try {
-            String randomValue = UUID.randomUUID().toString();
+            String lockVersion = UUID.randomUUID().toString();
             jedis = jedisPool.getResource();
+            // 自旋获取锁
             while (true){
-                String result = jedis.setex(lockName,DEFAULT_EXPIRE_TIME, randomValue);
-                if (SUCCESS.equals(result)){
-                    return;
+                // set EX 过期时间 NX 不存在key才进行设置
+                // EX 设置过期时间防止死锁
+                // NX 防止锁覆盖
+                SetParams setParams = new SetParams().nx().ex(DEFAULT_EXPIRE_TIME);
+                String result = jedis.set(lockName, lockVersion, setParams);
+                if (LOCK_SUCCESS.equals(result)){
+                    return lockVersion;
                 }
             }
         }catch (Exception e){
@@ -49,5 +67,35 @@ public class DistributedLock {
                 jedis.close();
             }
         }
+        return null;
+    }
+
+    /**
+     * 释放锁
+     * @param lockName
+     * @param lockVersion
+     * @return
+     */
+    public static boolean unLock(String lockName, String lockVersion){
+        try (Jedis jedis = jedisPool.getResource()) {
+            // watch监控key,若有其他事务修改key-value,事务不会被执行
+            // 防止锁被错误释放
+            jedis.watch(lockName);
+            // 检验锁版本，判断是否持有锁
+            if (lockVersion.equals(jedis.get(lockName))) {
+                // 开启事务
+                Transaction multi = jedis.multi();
+                // 释放锁
+                multi.del(lockName);
+                // 提交
+                List<Object> result = multi.exec();
+                if (CollectionUtils.isNotEmpty(result)) {
+                    return UNLOCK_SUCCESS.equals(result.size());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
--- src/test/java/com/brillilab/test/RedisTests.java
+++ src/test/java/com/brillilab/test/RedisTests.java
@@ -1,4 +1,58 @@
 package com.brillilab.test;
 
+import com.brillilab.redis.DistributedLock;
+import io.netty.util.concurrent.RejectedExecutionHandlers;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class RedisTests {
+
+    private final String LOCK = "lock_test";
+    private ThreadPoolExecutor threadPool;
+
+    @Before
+    public void prepareThreadPool(){
+        threadPool = new ThreadPoolExecutor(
+                20,
+                200,
+                60,
+                TimeUnit.SECONDS,new LinkedBlockingDeque<>(),
+                new ThreadPoolExecutor.AbortPolicy());
+    }
+
+    @Test
+    public void redisLockTest() throws InterruptedException {
+
+
+        threadPool.execute(() ->{
+            String lockVersion = DistributedLock.lock(LOCK);
+            System.out.println("Lock one ok");
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean b = DistributedLock.unLock(LOCK, lockVersion);
+            System.out.printf("Unlock one result : %s \n",b);
+        });
+
+        threadPool.execute(() ->{
+            String lockVersion = DistributedLock.lock(LOCK);
+            System.out.println("Lock two ok");
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean b = DistributedLock.unLock(LOCK, lockVersion);
+            System.out.printf("Unlock two result : %s \n",b);
+        });
+
+        threadPool.awaitTermination(50,TimeUnit.SECONDS);
+    }
 }
